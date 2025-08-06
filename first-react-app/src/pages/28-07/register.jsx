23ea46d6a3542d4b6b3e1b578a54e2cdba2695ae
Ashutosh Patil <ashutosh.patil1409@gmail.com>
2025-08-06 21:56:59 +0530

    Update register.jsx

--- first-react-app/src/pages/28-07/register.jsx
+++ first-react-app/src/pages/28-07/register.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Register() {
+  const [userDetails, setUserDetails] = useState({name: "", email:"", password:"", confPassword:""})
+
+const InputHandler = (event) => {
+  setUserDetails({...userDetails, [event.target.name]: event.target.value})
+}
+
+const Submit = (event) => {
+  event.preventDefault()
+
+}
   return (
     <div>
       <h1>This is the Register Page</h1>
-      <form>
-        <input type="text" placeholder="Enter your name" />
-        <input type="email" placeholder="Enter your email" />
-        <input type="password" placeholder="Create a password" />
-        <input type="password" placeholder="Confirm your password" />
+      <form onSubmit={Submit}>
+        <input id="name" name="name" type="text" placeholder="Enter your name" onChange={InputHandler}/>
+        <input id="email" name="email" type="email" placeholder="Enter your email" onChange={InputHandler}/>
+        <input id="password" name="password" type="password" placeholder="Create a password" onChange={InputHandler}/>
+        <input id="confPassword" name="confPassword" type="password" placeholder="Confirm your password" onChange={InputHandler}/>
         <button type="submit">Register</button>
       </form>
+      <div>
+        <h3>Name: {userDetails.name}</h3>
+        <h3>Email: {userDetails.email}</h3>
+        <h3>Password: {userDetails.password}</h3>
+        <h3>Confirm Password: {userDetails.confPassword}</h3>
+      </div>
     </div>
   );
 }
