ee8c91b93f771dcf721db8090b64ab69d015d8cd
Ashutosh Patil <ashutosh.patil1409@gmail.com>
2025-08-07 22:41:48 +0530

    Update to the Login and Register files

--- first-react-app/src/pages/28-07/login.jsx
+++ first-react-app/src/pages/28-07/login.jsx
@@ -1,12 +1,69 @@
 import React from "react";
+import { useState } from "react";
 
 function Login() {
+  const [userDetails, setUserDetails] = useState({ name: "", password: "" });
+
+  const [errors, setErrors] = useState({});
+
+  const InputHandler = (event) => {
+    setUserDetails({ ...userDetails, [event.target.name]: event.target.value });
+    setErrors({ ...errors, [event.target.name]: "" });
+  };
+
+  const Submit = (event) => {
+    event.preventDefault();
+
+    let newErrors = {};
+    if (!userDetails.name) newErrors.name = "Please enter your email";
+    if (!userDetails.password)
+      newErrors.password = "Please enter your password";
+
+    setErrors(newErrors);
+
+    if (userDetails.name && userDetails.password) {
+      alert(`Hi ${userDetails.name}, \nYou are logged in successfully`);
+      setUserDetails({ name: "", password: "" });
+    }
+  };
+
+  const [showPassword, setShowPassword] = useState("password");
+
+  const ShowPassword = () => {
+    setShowPassword("text");
+    setTimeout(() => {
+      setShowPassword("password");
+    }, 1500);
+  };
+
   return (
     <div>
       <h1>This is the Login Page</h1>
-      <form>
-        <input type="text" placeholder="Enter your name" />
-        <input type="password" placeholder="Enter your password" />
+      <form onSubmit={Submit}>
+        <input
+          name="name"
+          id="name"
+          type="text"
+          value={userDetails.name}
+          placeholder="Enter your name"
+          onChange={InputHandler}
+        />
+        {errors.name && <div style={{ color: "red" }}>{errors.name}</div>}
+        <div style={{ display: "flex" }}>
+          <input
+            name="password"
+            id="password"
+            value={userDetails.password}
+            type={showPassword}
+            placeholder="Enter your password"
+            onChange={InputHandler}
+          />
+          <button type="button" onClick={ShowPassword}>
+            Show
+          </button>
+        </div>
+        {errors.password && <div style={{ color: "red" }}>{errors.password}</div>}
+
         <button type="submit">Login</button>
       </form>
     </div>
--- first-react-app/src/pages/28-07/register.jsx
+++ first-react-app/src/pages/28-07/register.jsx
@@ -1,26 +1,125 @@
 import React, { useState } from "react";
 
-
 function Register() {
-  const [userDetails, setUserDetails] = useState({name: "", email:"", password:"", confPassword:""})
+  const [userDetails, setUserDetails] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confPassword: "",
+  });
 
-const InputHandler = (event) => {
-  setUserDetails({...userDetails, [event.target.name]: event.target.value})
-}
+  const [errors, setErrors] = useState({});
 
-const Submit = (event) => {
-  event.preventDefault()
+  const InputHandler = (event) => {
+    setUserDetails({ ...userDetails, [event.target.name]: event.target.value });
+    setErrors({ ...errors, [event.target.name]: "" });
+  };
 
-}
+  const Submit = (event) => {
+    event.preventDefault();
+
+    let newErrors = {};
+    if (!userDetails.name) newErrors.name = "Name is required";
+    if (!userDetails.email) newErrors.email = "Email is required";
+    if (!userDetails.password) newErrors.password = "Password is required";
+    if (!userDetails.confPassword)
+      newErrors.confPassword = "Confirmed Password is required";
+
+    setErrors(newErrors);
+
+    if (
+      userDetails.name &&
+      userDetails.email &&
+      userDetails.password &&
+      userDetails.confPassword
+    ) {
+      alert(`Registeration success! \nWelcome aboard ${userDetails.name}! `);
+      setUserDetails({ name: "", email: "", password: "", confPassword: "" });
+    }
+  };
+
+  const [showPassword, setShowPassword] = useState("password");
+
+  const ShowPassword = () => {
+    setShowPassword("text");
+    setTimeout(() => {
+      setShowPassword("password");
+    }, 1500);
+  };
+
+  const [showConfirmPassword, setShowConfirmPassword] = useState("password");
+
+  const ShowConfirmPassword = () => {
+    setShowConfirmPassword("text");
+    setTimeout(() => {
+      setShowConfirmPassword("password");
+    }, 1500);
+  };
+
   return (
     <div>
       <h1>This is the Register Page</h1>
       <form onSubmit={Submit}>
-        <input id="name" name="name" type="text" placeholder="Enter your name" onChange={InputHandler}/>
-        <input id="email" name="email" type="email" placeholder="Enter your email" onChange={InputHandler}/>
-        <input id="password" name="password" type="password" placeholder="Create a password" onChange={InputHandler}/>
-        <input id="confPassword" name="confPassword" type="password" placeholder="Confirm your password" onChange={InputHandler}/>
-        <button type="submit">Register</button>
+        <input
+          id="name"
+          value={userDetails.name}
+          name="name"
+          type="text"
+          placeholder="Enter your name"
+          onChange={InputHandler}
+        />
+        {errors.name && <div style={{ color: "red" }}>{errors.name}</div>}
+        <input
+          id="email"
+          value={userDetails.email}
+          name="email"
+          type="email"
+          placeholder="Enter your email"
+          onChange={InputHandler}
+        />
+        {errors.email && <div style={{ color: "red" }}>{errors.email}</div>}
+        <div style={{ display: "flex", flexDirection:"column" }}>
+          <div style={{display:"flex"}}>
+            <input
+              id="password"
+              value={userDetails.password}
+              name="password"
+              type={showPassword}
+              placeholder="Create a password"
+              onChange={InputHandler}
+            />
+
+            <button id="showPasswordBtn" type="button" onClick={ShowPassword}>
+              Show
+            </button>
+          </div>
+          {errors.password && (
+            <div style={{ color: "red" }}>{errors.password}</div>
+          )}
+        </div>
+        <div style={{ display: "flex", flexDirection:"column" }}>
+          <div style={{display:"flex"}}>
+            <input
+              id="confPassword"
+              value={userDetails.confPassword}
+              name="confPassword"
+              type={showConfirmPassword}
+              placeholder="Confirm your password"
+              onChange={InputHandler}
+            />
+            <button
+              id="showConfirnPasswordBtn"
+              type="button"
+              onClick={ShowConfirmPassword}
+            >
+              Show
+            </button>
+          </div>
+          {errors.confPassword && (
+            <div style={{ color: "red" }}>{errors.confPassword}</div>
+          )}
+        </div>
+        <input type="submit"></input>
       </form>
       <div>
         <h3>Name: {userDetails.name}</h3>
