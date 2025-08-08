9cf9718efbfe5fd22dd05e857a86d8d4666f349a
Ashutosh Patil <ashutosh.patil1409@gmail.com>
2025-08-09 00:27:41 +0530

    Create singleProductApi.jsx

---
+++ first-react-app/src/pages/08-08/singleProductApi.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SingleProductApi = () => {
+  return (
+    <>
+      <div className="product-container">
+        <div className="single-product-page-img">
+          <img src="" />
+        </div>
+        <div className="single-product-page-details">
+            <h2></h2>
+            <h4></h4>
+            <p></p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SingleProductApi;
